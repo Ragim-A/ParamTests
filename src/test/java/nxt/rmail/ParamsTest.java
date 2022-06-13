9dfe57d43284d5752fc6601f273212d9c530fcd1
Ragim <ratutnetu@gmail.com>
2022-06-13 23:36:18 +0300

    Начало

---
+++ src/test/java/nxt/rmail/ParamsTest.java
@@ -0,0 +1,12 @@
+package nxt.rmail;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class ParamsTest {
+    @ValueSource ();
+    @ParameterizedTest
+    void Test () {
+
+    }
+}
---
+++ src/test/java/nxt/rmail/SimpleTest.java
@@ -0,0 +1,14 @@
+package nxt.rmail;
+
+
+import org.junit.jupiter.api.Test;
+
+
+public class SimpleTest {
+
+    @Test
+    void simpleTest() {
+
+
+    }
+}
